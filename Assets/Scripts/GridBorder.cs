192a5c7c96b10576bfd4ea384cff81265a1b28f2
ozsune <ozsune@gmail.com>
2023-04-25 02:01:32 +0300

    refactor: change Grid Border

--- Assets/Scripts/GridBorder.cs
+++ Assets/Scripts/GridBorder.cs
@@ -4,6 +4,6 @@ public class GridBorder
     public const int MaxHeight = 50;
     public const int MaxWidth = 50;
 
-    public const int MinHeight = 2;
-    public const int MinWidth = 2;
+    public const int MinHeight = 3;
+    public const int MinWidth = 3;
 }
