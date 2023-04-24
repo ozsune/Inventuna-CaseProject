20493a101bc84e32aafb1f1ebd1cb21e94ffce3f
ozsune <ozsune@gmail.com>
2023-04-24 15:33:30 +0300

    fix: Player Pathfinding

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,19 +8,18 @@ public class PlayerController : MonoBehaviour, IPlaceable
 {
     public GameObject PlaceObject { get; private set; }
     public Tile CurrentTile { get; private set; }
+    public Tile DestinationTile { get; private set; }
     public ObjectPlacer Placer { get; private set; }
 
     void Awake()
     {
         PlaceObject = gameObject;
-
         Placer = new ObjectPlacer(this);
     }
 
     private void Start()
     {
         StartCoroutine(MovePosition());
-
     }
 
     public void SetTile(Tile tile)
@@ -30,18 +29,21 @@ public class PlayerController : MonoBehaviour, IPlaceable
     
     private IEnumerator MovePosition()
     {
-        Debug.Log(CurrentTile.TileObject.name);
-        var path = new PathFinder(CurrentTile, FindObjectOfType<EnemyController>().CurrentTile);
-        path.FindShortestPath();
+        DestinationTile = FindObjectOfType<EnemyController>().CurrentTile;
+        
+        var path = new PathFinder(CurrentTile, DestinationTile);
         var pathIndex = 0;
+        
+        path.FindHeuristicPath();
         
         while (true)
         {
             yield return new WaitForSeconds(0.1f);
             
             Placer.Place(path.heuristicPath[pathIndex], path.heuristicPath[pathIndex].Enabled);
-            pathIndex++;
-
+            
+            if(pathIndex < path.heuristicPath.Count - 1)
+                pathIndex++;
         }
     }
     
