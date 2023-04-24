bfce4b1f6ca138490d4ce976570a4a743332a02d
ozsune <ozsune@gmail.com>
2023-04-25 02:00:45 +0300

    refactor: serialize Player MovementDelay

--- Assets/Scripts/IPathFinder.cs
+++ Assets/Scripts/IPathFinder.cs
@@ -7,5 +7,6 @@ public interface IPathFinder
     public event Action onPathComplete;
     public event Action onPathFail;
     public bool Move { get; set; }
+    public float MovementDelay { get; set; }
     public IEnumerator MovePosition();
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour, IPlaceable, IPathFinder
 {
     public event Action onPathComplete;
-
+    public event Action onPathFail;
+    [field:SerializeField] public float MovementDelay { get; set; }
     public GameObject PlaceObject { get; private set; }
     public Tile CurrentTile { get; private set; }
     public Tile DestinationTile { get; private set; }
@@ -41,21 +42,26 @@ public class PlayerController : MonoBehaviour, IPlaceable, IPathFinder
 
         DestinationTile = FindObjectOfType<EnemyController>().CurrentTile;
         
-        var path = new PathFinder(CurrentTile, DestinationTile);
+        var path = new PathFinder();
         var pathIndex = 0;
-        path.FindPath();
+        path.FindPath(CurrentTile, DestinationTile);
         
         while (CurrentTile != DestinationTile)
         {
-            yield return new WaitForSeconds(.25f);
+            yield return new WaitForSeconds(MovementDelay);
+
+            if (path.finalPath.Count == 0)
+            {
+                onPathFail?.Invoke();
+                break;
+            }
             
             Placer.Place(path.finalPath[pathIndex].Tile);
-            
             pathIndex++;
-
+                
             if (CurrentTile == path.finalPath[^1].Tile)
             {
-                yield return new WaitForSeconds(0.5f);
+                yield return new WaitForSeconds(MovementDelay);
 
                 onPathComplete?.Invoke();
                 CurrentTile.Enabled = false;
